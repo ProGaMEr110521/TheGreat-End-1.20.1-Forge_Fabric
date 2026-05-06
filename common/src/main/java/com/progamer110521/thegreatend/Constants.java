959081e4fac29c9b51ae9e098794e6b2e07b0992
ProGaMEr110521 <vladtem3943@gmail.com>
2026-05-06 21:31:19 +0300

    Initial commit

---
+++ common/src/main/java/com/progamer110521/thegreatend/Constants.java
@@ -0,0 +1,15 @@
+package com.progamer110521.thegreatend;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class Constants {
+
+	public static final String MOD_ID = "thegreatend";
+	public static final String MOD_NAME = "thegreatend";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+
+	public static final List<String> SUPPORTED_LANGUAGES = List.of("RU_ru");
+}
---
+++ forge/src/main/java/com/progamer110521/thegreatend/mixin/MixinWinScreen.java
@@ -0,0 +1,10 @@
+package com.progamer110521.thegreatend.mixin;
+
+import net.minecraft.client.gui.screens.WinScreen;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(WinScreen.class)
+public class MixinWinScreen {
+
+
+}
